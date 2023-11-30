68a3da87d937b61c73e2fbfc77725ca13e460d07
mavles12345 <jackmavles@gmail.com>
2023-11-30 08:50:17 +0530

    Implicit wait added

--- src/test/java/org/base/BaseClass.java
+++ src/test/java/org/base/BaseClass.java
@@ -1,26 +1,33 @@
 package org.base;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseClass {
-	
-	
-	
-	
+
+	WebDriver driver;
+
 	public void initializeBrowser(String Browser) {
-		
-		if(Browser.equals("Chrome")) {
-			
-			WebDriver driver=new ChromeDriver();		
-		
-		}else if(Browser.equals("FireFox")) {
-			
-			WebDriver driver=new FirefoxDriver();
-			
+
+		if (Browser.equals("Chrome")) {
+
+			driver = new ChromeDriver();
+
+		} else if (Browser.equals("FireFox")) {
+
+			driver = new FirefoxDriver();
+
 		}
-		
+
+	}
+
+	public void implicitWait() {
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 	}
 
